6199dfc5a1e8e0c0b3d6c64826f09df87820103b
kathiravan-018 <kathir121006@gmail.com>
2026-07-29 23:38:41 +0530

    Drag error fixed

--- src/Components/Column.jsx
+++ src/Components/Column.jsx
@@ -7,12 +7,13 @@ import {
 import { useDroppable } from "@dnd-kit/core";
 
 function Column({ column, tasks, openModal, openEditModal }) {
+
   const { id, title } = column;
 
   const { setNodeRef } = useDroppable({
     id,
-    data: {
-      columnId: id,
+     data: {
+        columnId: id,
     },
   });
 
@@ -21,8 +22,6 @@ function Column({ column, tasks, openModal, openEditModal }) {
     if (title === "Doing") return "text-yellow-500";
     if (title === "Review") return "text-purple-700";
     if (title === "Done") return "text-green-500";
-
-    return "text-gray-700";
   }
 
   return (
@@ -50,10 +49,10 @@ function Column({ column, tasks, openModal, openEditModal }) {
       </SortableContext>
 
       <button
+        className="w-full border-2 border-dashed border-slate-300 rounded-xl py-3 cursor-pointer hover:bg-white/40 hover:text-blue-400 transition-all duration-300"
         onClick={() => openModal(id)}
-        className="w-full border-2 border-dashed border-slate-300 rounded-xl py-3 cursor-pointer hover:bg-white/40 hover:text-blue-500 transition-all duration-300"
       >
-        <FaPlus className="inline mr-2 pb-1" />
+        <FaPlus className="inline mr-2 pb-1 " />
         Add Task
       </button>
     </div>
