3983ca409fc5beb82ecd2190be493e390122a68a
kathiravan-018 <kathir121006@gmail.com>
2026-07-29 23:42:50 +0530

    Drag error fixed

--- src/Components/TaskCardOverlay.jsx
+++ src/Components/TaskCardOverlay.jsx
@@ -1,44 +1,36 @@
-import { SlCalender } from "react-icons/sl";
-
 function TaskCardOverlay({ task }) {
 
   function getPriorityColor(priority) {
-    if (priority === "High") return "bg-red-100 text-red-700";
-    if (priority === "Medium") return "bg-yellow-100 text-yellow-700";
+    if (priority === "High") {
+      return "bg-red-100 text-red-700";
+    }
+
+    if (priority === "Medium") {
+      return "bg-yellow-100 text-yellow-700";
+    }
+
     return "bg-green-100 text-green-700";
   }
 
-  function formatDate(date) {
-    return new Date(date).toLocaleDateString("en-US", {
-      month: "short",
-      day: "numeric",
-    });
-  }
 
   return (
-    <div className="bg-white rounded-xl p-3 shadow-xl w-[260px] rotate-2 opacity-90">
+    <div className="w-[280px] bg-white rounded-xl p-4 shadow-xl rotate-3">
 
-      <h3 className="font-semibold">
+      <h3 className="text-lg font-semibold break-all">
         {task.title}
       </h3>
 
-      <p className="text-sm text-gray-600 mt-2">
+      <p className="text-gray-500 text-sm mt-2 break-all line-clamp-3">
         {task.description}
       </p>
 
-      <div className="flex justify-between mt-4">
 
+      <div className="mt-4">
         <span
-          className={`px-3 py-1 rounded-full ${getPriorityColor(task.priority)}`}
+          className={`px-3 py-2 rounded-full text-sm font-medium ${getPriorityColor(task.priority)}`}
         >
           {task.priority}
         </span>
-
-        <div className="flex items-center gap-1">
-          <SlCalender />
-          {formatDate(task.due_date)}
-        </div>
-
       </div>
 
     </div>
